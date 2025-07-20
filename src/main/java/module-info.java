1c158c37c0d76d08e0f12d0a4947d613b7ec6427
Umaid Nasir <info.umaid@gmail.com>
2025-07-20 18:47:01 -0400

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.assignment2gc200579623 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.assignment2gc200579623 to javafx.fxml;
+    exports com.example.assignment2gc200579623;
+}
