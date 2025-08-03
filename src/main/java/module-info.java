e02e96e0f3d75663c489dcc3608d26710a1ee151
Umaid Nasir <info.umaid@gmail.com>
2025-08-03 19:25:12 -0400

    final review

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.assignment2gc200579623 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
+    requires org.json;
 
 
     opens com.example.assignment2gc200579623 to javafx.fxml;
